7bb407a96e3bfff4d1d87b196e920d1464c7a56d
JHU-ForrestVanduren <fvandur1@jh.edu>
2025-10-16 13:32:53 -0600

    Makes socket origin dynamic

--- gameBoard/static/gameBoard/javascript/script.js
+++ gameBoard/static/gameBoard/javascript/script.js
@@ -1,6 +1,8 @@
 const currentPath = window.location.pathname;
 const gameId = currentPath.substring(currentPath.lastIndexOf('/') +1);
-let socket = new WebSocket(`ws://127.0.0.1:8000/ws/game/${gameId}`);
+let currentOrigin = window.location.origin;
+currentOrigin = currentOrigin.substring(currentOrigin.indexOf('/'));
+let socket = new WebSocket(`ws:${currentOrigin}/ws/game/${gameId}`);
 
 let test2 = document.querySelector('#boardSquare');
 
