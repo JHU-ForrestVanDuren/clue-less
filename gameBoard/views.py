a62178ee37b77a8ba2d4bd92f2c71b137e58db42
JHU-ForrestVanduren <fvandur1@jh.edu>
2025-10-16 18:20:59 -0600

    Adds functionality to show different character images based on the current players chosen character

--- gameBoard/views.py
+++ gameBoard/views.py
@@ -2,15 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from chatSystem.models import Messages
 from lobby.models import ActiveGames
+from players.models import Players
 from django.views.decorators.cache import cache_control
 
 @cache_control(no_cache=True, no_store=True, must_revalidate=True)
 def index(request, game_id):
     game = ActiveGames.objects.get(id=game_id)
     messages = Messages.objects.filter(game=game)
-    print(messages, flush=True)
-    print('test', flush=True)
+    playerId = request.COOKIES.get('playerId')
+    player = Players.objects.get(id=playerId)
+
     context = {
-        "messages": messages
+        "messages": messages,
+        "player": player
     }
     return render(request, "gameBoard/index.html", context)
